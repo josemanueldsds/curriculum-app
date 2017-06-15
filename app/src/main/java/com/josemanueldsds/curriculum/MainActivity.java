e4294a662894ea4d104b821f88d9294efbc9e77f
Jose Manuel De Sousa <josemanueldsds@gmail.com>
2017-06-15 13:16:16 -0400

    Created navigation drawables

--- app/src/main/java/com/josemanueldsds/curriculum/MainActivity.java
+++ app/src/main/java/com/josemanueldsds/curriculum/MainActivity.java
@@ -11,27 +11,6 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,7 +18,25 @@ public class MainActivity extends AppCompatActivity {
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_profile:
+                        mTextMessage.setText("Profile");
+                        return true;
+                    case R.id.navigation_works:
+                        mTextMessage.setText("Works");
+                        return true;
+                    case R.id.navigation_apps:
+                        mTextMessage.setText("Apps");
+                        return true;
+                    case R.id.navigation_studies:
+                        mTextMessage.setText("Studies");
+                        return true;
+                }
+                return false;
+            }
+        });
     }
-
 }
